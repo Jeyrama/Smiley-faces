ac79f405ec2630b3b07eea12273d3efef2d70e5a
Jey <126204426+Jeyrama@users.noreply.github.com>
2024-12-09 19:22:37 -0800

    Update Smiley-faces.js

--- Smiley-faces.js
+++ Smiley-faces.js
@@ -24,3 +24,17 @@ Note:
 
 
 // Solution
+
+function countSmileys(arr) {
+  let smileys = [":)",";)",":-)",";-)",";~)",":~)",":D",";D",":-D",":~D",";-D",";~D"];
+  let count = 0;
+  
+  for (let i=0; i<arr.length; i++){
+    for (let j=0; j<smileys.length; j++){
+      if (arr[i]===smileys[j]){
+        count++;
+      }
+    }
+    }
+  return count;
+  }
